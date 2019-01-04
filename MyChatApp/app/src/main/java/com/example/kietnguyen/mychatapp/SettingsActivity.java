5290a10e57b50e381b08284ec28d0a67fcde0832
Kiet Nguyen <roynguyen147@gmail.com>
2019-01-04 19:44:05 +0700

    add crop image and update image url on firebase

--- MyChatApp/app/src/main/java/com/example/kietnguyen/mychatapp/SettingsActivity.java
+++ MyChatApp/app/src/main/java/com/example/kietnguyen/mychatapp/SettingsActivity.java
@@ -1,14 +1,21 @@
 package com.example.kietnguyen.mychatapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,6 +23,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -25,9 +37,17 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView userStatus;
     private Button btnChangeAvatar;
     private Button btnChangeStatus;
+    //for get information about user
     private FirebaseUser mCurrentUser;
+    //for get data from realtime db from firebase
     private DatabaseReference mUserDatabase;
+    // for get files from Firebase Storage
+    private StorageReference mFirebaseStorage;
+    private ProgressDialog mProgressDialog;
+
+
     private final static int GALLERY_PICK = 1;
+    private String uid;
 
 
     @Override
@@ -40,11 +60,13 @@ public class SettingsActivity extends AppCompatActivity {
         userStatus = findViewById(R.id.settings_status);
         btnChangeAvatar = findViewById(R.id.settings_changeImage);
         btnChangeStatus = findViewById(R.id.settings_changeStatus);
+
+        mFirebaseStorage = FirebaseStorage.getInstance().getReference();
 //        get current user logged in
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
-        String current_uid = mCurrentUser.getUid();
+        uid = mCurrentUser.getUid();
 //        get data of current user from this node
-        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
 //        get data from firebase
         mUserDatabase.addValueEventListener(new ValueEventListener() {
             @Override
@@ -75,13 +97,70 @@ public class SettingsActivity extends AppCompatActivity {
         btnChangeAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                open the windows for user to select which image they want to use
                 Intent galleryIntent = new Intent();
                 galleryIntent.setType("image/*");
                 galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-
                 startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
+
             }
         });
 
+    }
+
+//    function receive selected image of user then do crop
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_PICK && resultCode == RESULT_OK){
+
+            //show the progressdiaglog to let user wait a little bit
+            mProgressDialog = new ProgressDialog(SettingsActivity.this);
+            mProgressDialog.setTitle("Waiting");
+            mProgressDialog.setMessage("Wait a minute for update your avatar");
+//            disable tap outside to cancel
+            mProgressDialog.setCanceledOnTouchOutside(false);
+            mProgressDialog.show();
+
+            Uri imageUri = data.getData();
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1) /*set initial size to crop image*/
+                    .start(this);
+
+        }
+//        get the cropped image uri
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+//                uri of the cropped image being pushed to firebase storage
+                Uri resultUri = result.getUri();
+//                named the image file by uid
+                final StorageReference filepath = mFirebaseStorage.child("profile_images").child(uid + ".jpg");
+//                put image file to firebase storage
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String download_uri = uri.toString();
+                                    mUserDatabase.child("image").setValue(download_uri);
+                                    mProgressDialog.dismiss();
+                                    Log.d("TAG", download_uri);
+                                    Toast.makeText(SettingsActivity.this, "Update image url succe", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }else {
+                            mProgressDialog.hide();
+                            Toast.makeText(SettingsActivity.this, "Update image url failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
